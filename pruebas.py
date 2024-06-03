fe3a7ec44a210baa48418d370ea509beffcb33cb
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-06-03 12:00:08 -0500

    NUEVO MENU

---
+++ pruebas.py
@@ -0,0 +1,12 @@
+# Definir caracteres para diferentes segmentos del círculo
+segmentos_circulo = [
+    "  ⬤  ",  # Parte superior
+    "⬤   ⬤",  # Parte media
+    "  ⬤  "   # Parte inferior
+]
+
+# Imprimir el círculo en la consola
+for linea in segmentos_circulo:
+    print(linea)
+
+
