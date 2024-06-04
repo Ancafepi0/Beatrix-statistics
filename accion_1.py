d3620b8ad1e854af5e58fd7d5958ea3135f41e6a
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-06-03 19:13:27 -0500

    ACCION 1 Y 2 TOTALMENTE TERMINADAS

--- accion_1.py
+++ accion_1.py
@@ -31,9 +31,9 @@ class accion_1:
                             str(valor_frp),
                             str(valor_frpa),
                             str(valor_fo)]) 
-            print ("\033["+"7;30;45"+"m "+"|"+" "* tamano +"|"+" "* tamano+"|"+" "* tamano+"|"+" "* tamano+"|"+" "* tamano+"|"+" "* tamano+"|"+" "* tamano+"|"+" \033[0m")
+            print ("\033["+"7;30;45"+"m "+"\u2503"+" "* tamano +"\u2503"+" "* tamano+"\u2503"+" "* tamano+"\u2503"+" "* tamano+"\u2503"+" "* tamano+"\u2503"+" "* tamano+"\u2503"+" "* tamano+"\u2503"+" \033[0m")
             print (linea)
-            print ("\033["+"7;30;45"+"m "+" -"+"-"* (tamano*7)+"----- "+" \033[0m")
+            print ("\033["+"7;30;45"+"m "+"\u2523"+("\u2501"* (tamano)+"\u254B")*6+("\u2501"*tamano)+"\u252B"+" \033[0m")
 
     def saber_mayor (self, intervalos):
         tamaño_mayor = 0
@@ -54,38 +54,38 @@ class accion_1:
         if (tamaño_mayor < 12):
             tamaño_mayor = 12
 
-        indice= "".join ([str("| "+"INTERVALO".center(tamaño_mayor-2," ")+" |"),
-                            str(" "+"FRECUENCIA".center(tamaño_mayor-2," ")+" | "),
-                            str(" "+"FR".center(tamaño_mayor-2," ")+"| "),
-                            str(" "+"FRA".center(tamaño_mayor-2," ")+"| "),
-                            str(" "+"FRP".center(tamaño_mayor-2," ")+"| "),
-                            str(" "+"FRPA".center(tamaño_mayor-2," ")+"| "),
-                            str(" "+"F°".center(tamaño_mayor-2," ")+"|")])
-        linea_superior = "|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"+" "*(tamaño_mayor)+"|"
-        print ("\033["+"7;30;45"+"m "+" _"+"_"*(tamaño_mayor*7)+"_____ "+" \033[0m")
+        indice= "".join ([str("\u2503 "+"INTERVALO".center(tamaño_mayor-2," ")+" \u2503"),
+                            str(" "+"FRECUENCIA".center(tamaño_mayor-2," ")+" \u2503 "),
+                            str(" "+"FR".center(tamaño_mayor-2," ")+"\u2503 "),
+                            str(" "+"FRA".center(tamaño_mayor-2," ")+"\u2503 "),
+                            str(" "+"FRP".center(tamaño_mayor-2," ")+"\u2503 "),
+                            str(" "+"FRPA".center(tamaño_mayor-2," ")+"\u2503 "),
+                            str(" "+"F°".center(tamaño_mayor-2," ")+"\u2503")])
+        linea_superior = "\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"+" "*(tamaño_mayor)+"\u2503"
+        print ("\033["+"7;30;45"+"m "+"\u250F"+("\u2501"*(tamaño_mayor)+"\u2533")*6+"\u2501"*tamaño_mayor+"\u2513"+" \033[0m")
         print ("\033["+"7;30;45"+"m "+linea_superior+" \033[0m")
         print ("\033["+"7;30;45"+"m "+indice+" \033[0m")
-        print ("\033["+"7;30;45"+"m "+" -"+"-"*(tamaño_mayor*7)+"----- "+" \033[0m")       
+        print ("\033["+"7;30;45"+"m "+"\u2523"+("\u2501"*(tamaño_mayor)+"\u254B")*6+("\u2501")*tamaño_mayor+"\u252B"+" \033[0m")       
     #SIRVE PARA ACOMODAR EL TAMAÑO DEL DATO A AGREGAR EN VALORES DIFERENTES AL PRIMERO Y AL ULTIMO
     def modificador_dibujar_general (self,dato_acomodar,tamano):
         dato_acomodar = str(dato_acomodar)
         if (tamano < 12):
             tamano = 12
-        mensaje="\033["+"7;30;45"+"m"+dato_acomodar.center(tamano-1," ")+"|"+" \033[0m"
+        mensaje="\033["+"7;30;45"+"m"+dato_acomodar.center(tamano-1," ")+"\u2503"+" \033[0m"
         return (mensaje)
     #SIRVE PARA ACOMODAR EL TAMAÑO DEL PRIMER DATO
     def modificador_dibujar_primario (self,dato_acomodar,tamano):
         dato_acomodar = str(dato_acomodar)
         if (tamano < 12):
             tamano = 12
-        mensaje="\033["+"7;30;45"+"m"+" |"+dato_acomodar.center(tamano," ")+"|"+" \033[0m"
+        mensaje="\033["+"7;30;45"+"m"+" \u2503"+dato_acomodar.center(tamano," ")+"\u2503"+" \033[0m"
         return (mensaje)
     #SIRVE PARA ACOMODAR EL TAMAÑO DEL ULTIMO DAÑO
     def modificador_dibujar_ultimo (self,dato_acomodar,tamano):
         dato_acomodar = str(dato_acomodar)
         if (tamano < 12):
             tamano = 12
-        mensaje="\033["+"7;30;45"+"m"+dato_acomodar.center(tamano-1," ")+"|"+" \033[0m"
+        mensaje="\033["+"7;30;45"+"m"+dato_acomodar.center(tamano-1," ")+"\u2503"+" \033[0m"
         return (mensaje)
 
 #INSTACIA DE LA CLASE ACCION_1
