2ba2070520b423bb10289571d0a47a78e4c84ac1
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-06-05 10:34:06 -0500

    GRAFICO 5 SUPERCOMLPETADO

--- accion_5.py
+++ accion_5.py
@@ -30,23 +30,23 @@ class accion_5:
                     forma_linea = "\u26AA".center(espacio," ") 
                     mensaje.append(forma_linea)
                 elif (max_valor<lista_para_bolitas[j]):
-                    forma_linea = "\u26AA".center(espacio-1," ") 
+                    forma_linea = "\u26AA".center(espacio," ") 
                     mensaje.append(forma_linea)
                 elif (max_valor>lista_para_bolitas[j]):
-                    forma_linea = "\u2800".center(espacio," ")
+                    forma_linea = "\u2800".center(espacio+1," ")
                     mensaje.append(forma_linea)
                 else :
                     forma_linea = "\u2800".center(espacio," ")
                     mensaje.append(forma_linea)    
             max_valor-=1     
             linea_contructura= ("") .join (mensaje)
-            print ("\033["+"7;30;45"+"m "+ " ".center(espacio_inter," ") +" "+linea_contructura+" ".center(espacio_inter+1,"\u2800")+"\033[0m")
-        print ("\033["+"7;30;45"+"m "+" ".center(espacio_inter," ")+("\u2501")+(("\u2501")+('\u2501')+('\u2501'))*(len(lista_acomodar))+" ".center(espacio_inter-1," ")+"\033[0m")
+            print ("\033["+"7;30;45"+"m "+ " ".center(espacio_inter," ") +" "+linea_contructura+" ".center(espacio_inter,"\u2800")+"\033[0m")
+        print ("\033["+"7;30;45"+"m "+" ".center(espacio_inter," ")+("\u2501")+(("\u2501")+('\u2501')+('\u2501')+('\u2501'))*(len(lista_acomodar))+" ".center(espacio_inter," ")+"\033[0m")
         for i in range (len(lista_con_intervalos)):
-            lista_con_intervalos[i]=str(lista_con_intervalos[i]).center(espacio_inter+1,"\u2800")
-        linea_intervalos= ("  ") .join (lista_con_intervalos)
+            lista_con_intervalos[i]=str(lista_con_intervalos[i]).center(espacio+1,"\u2800")
+        linea_intervalos= ("") .join (lista_con_intervalos)
 
-        print ("\033["+"7;30;45"+"m"+" ".center(espacio+1,"\u2800")+linea_intervalos+" ".center(espacio-1,"\u2800")+"\033[0m")
+        print ("\033["+"7;30;45"+"m"+" ".center(espacio,"\u2800")+linea_intervalos+" ".center(espacio-4,"\u2800")+"\033[0m")
 
     def acomodar_lista(self, intervalos):
     # Eliminar duplicados convirtiendo la lista a un conjunto
@@ -62,5 +62,5 @@ class accion_5:
     
         return lista_ordenada 
 ejecutar_5 = accion_5()
-intervalos = [['1',2,1,3,4,5,14],['1',4,1,45,89,456,1234],['1',5,1,45,89,456,1234],['1',4,4,45,89,456,1234],['1',5,1,45,89,456,1234],['1',5,1,45,89,456,1234]]
+intervalos = [['1',7,1,3,4,5,14],['1',2,1,45,89,456,1234],['1',5,1,45,89,456,1234],['1',4,4,45,89,456,1234],['1',5,1,45,89,456,1234],['1',5,1,45,89,456,1234]]
 ejecutar_5.dibujar(intervalos)
