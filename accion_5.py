e0bdbac62fa6bd1adb8526f4e63c836c6702650f
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-06-05 10:25:15 -0500

    COMPLETADISIMO VERSION 5

--- accion_5.py
+++ accion_5.py
@@ -4,33 +4,63 @@ class accion_5:
         pass
 
     def dibujar (self,intervalos):
-        for intervalo in (intervalos):
-            valor_intervalo= intervalo [0]
-            valor_intervalo= self.modificador_dibujar_primario (valor_intervalo)
-            valor_frecuencia=  intervalo [1]
-            valor_frecuencia= self.modificador_dibujar_general (valor_frecuencia)
-            valor_fr= intervalo [2]
-            valor_fr= self.modificador_dibujar_general (valor_fr)
-            valor_fra= intervalo [3]
-            valor_fra= self.modificador_dibujar_general (valor_fra)
-            valor_frp= intervalo [4]
-            valor_frp= self.modificador_dibujar_general (valor_frp)
-            valor_frpa= intervalo [5]
-            valor_frpa= self.modificador_dibujar_general (valor_frpa)
-            valor_fo= intervalo [6]
-            valor_fo= self.modificador_dibujar_ultimo(valor_fo)
-        #JOIN UTILIZADO PARA GUARDAR EL MENSAJE CON CADA UNO DE LOS DATOS SEPARADOS POR UN |
-            linea= "     ".join ([str(valor_intervalo),
-                            str(valor_frecuencia),
-                            str(valor_fr),
-                            str(valor_fra),
-                            str(valor_frp),
-                            str(valor_frpa),
-                            str(valor_fo)])
-            print ("\033["+"7;30;45"+"m "+"|            |            |            |            |            |            |            |"+" \033[0m")
-            print (linea)
-            print ("\033["+"7;30;45"+"m "+" ------------------------------------------------------------------------------------------ "+" \033[0m")
+        lista_acomodar = [sublista[1] for sublista in intervalos]
+        lista_con_intervalos= self.acomodar_lista(lista_acomodar)
+        intervalo_mas_grande= max(lista_con_intervalos)
+        espacio_inter=2
+        espacio=5
 
+        lista_para_bolitas = []
+        if (len(str(intervalo_mas_grande))>1):
+            espacio_inter= len(str(intervalo_mas_grande))
+
+        for i in range (len(lista_con_intervalos)):
+            contador=0         
+            for j in range (len(intervalos)): 
+                if (intervalos[j][1]==lista_con_intervalos[i]):
+                     contador += 1 
+            lista_para_bolitas.append(contador)
+
+        max_valor = max(lista_para_bolitas)
+        for i in range (max_valor):         
+            mensaje= []
+            
+            for j in range (len(lista_con_intervalos)): 
+                if (max_valor==lista_para_bolitas[j]):
+                    forma_linea = "\u26AA".center(espacio," ") 
+                    mensaje.append(forma_linea)
+                elif (max_valor<lista_para_bolitas[j]):
+                    forma_linea = "\u26AA".center(espacio-1," ") 
+                    mensaje.append(forma_linea)
+                elif (max_valor>lista_para_bolitas[j]):
+                    forma_linea = "\u2800".center(espacio," ")
+                    mensaje.append(forma_linea)
+                else :
+                    forma_linea = "\u2800".center(espacio," ")
+                    mensaje.append(forma_linea)    
+            max_valor-=1     
+            linea_contructura= ("") .join (mensaje)
+            print ("\033["+"7;30;45"+"m "+ " ".center(espacio_inter," ") +" "+linea_contructura+" ".center(espacio_inter+1,"\u2800")+"\033[0m")
+        print ("\033["+"7;30;45"+"m "+" ".center(espacio_inter," ")+("\u2501")+(("\u2501")+('\u2501')+('\u2501'))*(len(lista_acomodar))+" ".center(espacio_inter-1," ")+"\033[0m")
+        for i in range (len(lista_con_intervalos)):
+            lista_con_intervalos[i]=str(lista_con_intervalos[i]).center(espacio_inter+1,"\u2800")
+        linea_intervalos= ("  ") .join (lista_con_intervalos)
+
+        print ("\033["+"7;30;45"+"m"+" ".center(espacio+1,"\u2800")+linea_intervalos+" ".center(espacio-1,"\u2800")+"\033[0m")
+
+    def acomodar_lista(self, intervalos):
+    # Eliminar duplicados convirtiendo la lista a un conjunto
+        conjunto_intervalos = set(intervalos)
+        lista_ordenada = list(conjunto_intervalos)
+    
+    # Implementar el algoritmo de ordenamiento burbuja para ordenar de mayor a menor
+        n = len(lista_ordenada)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if lista_ordenada[j] > lista_ordenada[j+1]:  
+                    lista_ordenada[j], lista_ordenada[j+1] = lista_ordenada[j+1], lista_ordenada[j]
+    
+        return lista_ordenada 
 ejecutar_5 = accion_5()
-intervalos = [['1',2,1,3,4,5,14],['1',1,1,45,89,456,1234]]
+intervalos = [['1',2,1,3,4,5,14],['1',4,1,45,89,456,1234],['1',5,1,45,89,456,1234],['1',4,4,45,89,456,1234],['1',5,1,45,89,456,1234],['1',5,1,45,89,456,1234]]
 ejecutar_5.dibujar(intervalos)
